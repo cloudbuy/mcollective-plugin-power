8c016af88601dcea499c178f340d8ba4d01cdf1b
Damien Churchill <damoxc@gmail.com>
2018-01-29 11:53:36 +0000

    add support for Windows

--- agent/power.rb
+++ agent/power.rb
@@ -2,20 +2,42 @@ module MCollective
   module Agent
     class Power<RPC::Agent
 
+      def shutdown_command
+        if MCollective::Util.windows?
+          if File.exists?("#{ENV['SYSTEMROOT']}\\sysnative\\shutdown.exe")
+            "#{ENV['SYSTEMROOT']}\\sysnative\\shutdown.exe"
+          elsif File.exists?("#{ENV['SYSTEMROOT']}\\system32\\shutdown.exe")
+            "#{ENV['SYSTEMROOT']}\\system32\\shutdown.exe"
+          else
+            'shutdown.exe'
+          end
+        else
+          '/sbin/shutdown'
+        end
+      end
+
       action "shutdown" do
-	out = ""
-	run("/sbin/shutdown -h now", :stdout => out, :chomp => true )
-	reply[:output] = "Shutdown initiated"
+        out = ""
+        if MCollective::Util.windows?
+          run([shutdown_command, '/s', '/f'], :stdout => out, :chomp => true)
+        else
+          run("/sbin/shutdown -h now", :stdout => out, :chomp => true )
+        end
+        reply[:output] = "Shutdown initiated"
       end
 
       action "reboot" do
-	out = ""
-	run("/sbin/shutdown -r now", :stdout => out, :chomp => true )
-	reply[:output] = "Reboot initiated"
+        out = ""
+        if MCollective::Util.windows?
+          run([shutdown_command, '/r', '/f'], :stdout => out, :chomp => true)
+        else
+          run("/sbin/shutdown -r now", :stdout => out, :chomp => true )
+        end
+        reply[:output] = "Reboot initiated"
       end
 
     end
   end
 end
 
-# vi:tabstop=2:expandtab:ai:filetype=ruby
+# vi:tabstop=2:softtabstop=2:shiftwidth=2:expandtab:ai:filetype=ruby
